9441b52cd8662f85d23fa4f00c4b7b7b5dfc7531
sehee96126 <45127144+sehee96126@users.noreply.github.com>
2019-05-18 17:09:20 +0900

    botton_nav_menu 생성

---
+++ app/src/main/java/com/example/kioskclient/Activity_Home.java
@@ -0,0 +1,46 @@
+package com.example.kioskclient;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Activity_Home extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("홈 화면");
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText("즐겨찾기 화면");
+                    return true;
+                case R.id.navigation_shopping_cart:
+                    mTextMessage.setText("주문내역 화면");
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText("마이페이지 화면");
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/example/kioskclient/MainActivity.java
+++ app/src/main/java/com/example/kioskclient/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.kioskclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.Button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent;
+        intent = new Intent(this,Activity_Home.class);
+        startActivity(intent);
+
     }
 }
