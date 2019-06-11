ef87c64054be4e0973d1c5ad403d1364344b7ddb
박진아 <bagjin-a@bagjin-aui-MacBook-Pro.local>
2019-06-11 14:59:09 +0900

    위치 서비스 추가

--- app/src/main/java/com/example/kioskclient/FragmentMenuHome.java
+++ app/src/main/java/com/example/kioskclient/FragmentMenuHome.java
@@ -1,15 +1,26 @@
 package com.example.kioskclient;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,13 +28,19 @@ import android.widget.TextView;
 public class FragmentMenuHome extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     Button searchBtn, backHomeBtn, deleteBtn;
+    ImageButton locationBtn;
     EditText searchText;
-    TextView userSearchText, searchInMsg, resultNoRest;
-    ListView listItems;
+    TextView userSearchText, listText, resultNoRest, nowLocation;
+    ListView listItems, favoriteItems;
     ListViewSearchAdapter adapter;
+    UserLocationUpdate.GeoPoint geoPoint;
+    FavoriteListViewAdapter favoriteListViewAdapter;
+
+    private UserLocationUpdate userLocationUpdate;
+    private Context mContext;
+
     public FragmentMenuHome() {
         // Required empty public constructor
-
     }
 
     @Override
@@ -31,28 +48,49 @@ public class FragmentMenuHome extends Fragment implements View.OnClickListener, 
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_menu_home, container, false);
+        mContext = container.getContext();
 
         searchBtn = (Button) view.findViewById(R.id.searchBtn);
         backHomeBtn = (Button) view.findViewById(R.id.backHomeBtn);
         deleteBtn = (Button) view.findViewById(R.id.deleteBtn);
         searchText = (EditText) view.findViewById(R.id.searchText);
         listItems = (ListView) view.findViewById(R.id.listItems);
+        favoriteItems = (ListView) view.findViewById(R.id.favoriteItems);
         userSearchText = (TextView) view.findViewById(R.id.userSearchText);
-        searchInMsg = (TextView) view.findViewById(R.id.searchInMsg);
         resultNoRest = (TextView) view.findViewById(R.id.resultNoRest);
+        listText = (TextView) view.findViewById(R.id.listText);
+        locationBtn = (ImageButton) view.findViewById(R.id.now_location);
+        nowLocation = (TextView) view.findViewById(R.id.now_location_text);
+
+
+        if (!((MainActivity)mContext).checkLocationServicesStatus()) {
+            ((MainActivity)mContext).showDialogForLocationServiceSetting();
+        }else {
+            ((MainActivity)mContext).checkRunTimePermission();
+        }
+
+        userLocationUpdate = new UserLocationUpdate(mContext);
 
         adapter = new ListViewSearchAdapter();
         listItems.setAdapter(adapter);
 
+        favoriteListViewAdapter = new FavoriteListViewAdapter();
+        favoriteItems.setAdapter(favoriteListViewAdapter);
+
         for(int i = 0; i < 10; i++){
             adapter.addItem("example XX점", (i+1.0), getResources().getDrawable(R.drawable.star_score), "garlic, fried chicken");
             adapter.addItem("test YY점", (i+1.0), getResources().getDrawable(R.drawable.star_score), "pizza, potato");
+            favoriteListViewAdapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_baseline_add_shopping_cart_24px),
+                    "매장"+ i, "아메리카노" +i);
         }
+        searchText.setText("");
 
         backHomeBtn.setOnClickListener(this);
         deleteBtn.setOnClickListener(this);
         searchBtn.setOnClickListener(this);
         listItems.setOnItemClickListener(this);
+        favoriteItems.setOnItemClickListener(this);
+        locationBtn.setOnClickListener(this);
 
         return view;
     }
@@ -62,23 +100,26 @@ public class FragmentMenuHome extends Fragment implements View.OnClickListener, 
         if(v.getId() == R.id.backHomeBtn){
             getActivity().findViewById(R.id.navigation_home).performClick();
         } else if(v.getId() == R.id.deleteBtn){
+            // 검색 후 x 버튼 클릭 시
             backHomeBtn.setVisibility(View.VISIBLE);
             userSearchText.setVisibility(View.GONE);
             searchText.setVisibility(View.VISIBLE);
             deleteBtn.setVisibility(View.GONE);
             searchBtn.setVisibility(View.VISIBLE);
             listItems.setVisibility(View.GONE);
+            favoriteItems.setVisibility(View.VISIBLE);
             resultNoRest.setVisibility(View.GONE);
-            searchInMsg.setVisibility(View.VISIBLE);
+            listText.setText("즐겨찾기 목록");
 
             searchText.setText("");
         } else if(v.getId() == R.id.searchBtn){
+            // 검색 버튼 클릭 시
             backHomeBtn.setVisibility(View.GONE);
             userSearchText.setVisibility(View.VISIBLE);
             searchText.setVisibility(View.GONE);
             deleteBtn.setVisibility(View.VISIBLE);
             searchBtn.setVisibility(View.GONE);
-            searchInMsg.setVisibility(View.GONE);
+            favoriteItems.setVisibility(View.GONE);
 
             if(searchText.getText().toString().length() > 0 )
                 listItems.setFilterText(searchText.getText().toString());
@@ -86,6 +127,7 @@ public class FragmentMenuHome extends Fragment implements View.OnClickListener, 
                 listItems.clearTextFilter();
 
             userSearchText.setText(searchText.getText().toString() + "\t\t" + adapter.getCount() + "개");
+            listText.setText("결과 목록");
 
             if(adapter.getCount() > 0) {
                 listItems.setVisibility(View.VISIBLE);
@@ -94,11 +136,41 @@ public class FragmentMenuHome extends Fragment implements View.OnClickListener, 
                 listItems.setVisibility(View.GONE);
                 resultNoRest.setVisibility(View.VISIBLE);
             }
+        } else if(v.getId() == R.id.now_location){
+            // 현재 위치 재설정 버튼 클릭 시
+            try {
+                double latitude = userLocationUpdate.getLatitude();
+                double longitude = userLocationUpdate.getLongitude();
+                String address = userLocationUpdate.getCurrentAddress(35.228545, 128.889352);
+                Toast.makeText(mContext,"현재위치 \n위도 " + latitude + "\n경도 " + longitude, Toast.LENGTH_LONG).show();
+                nowLocation.setText(address);
+            } catch (IOException e) {
+                Toast.makeText(mContext, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        getActivity().findViewById(R.id.navigation_shopping_cart).performClick();
+        //리스트 클릭 시 페이지 이동 -> 각 store로 이동할 수 있도록 작업 필요 / 현재는 fragmentstorehome 페이지가 출력되도로 작업해둠
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        FragmentStoreHome fragmentStoreHome = new FragmentStoreHome();
+        /* 현재 위치를 기준으로 해당 가게와 거리 계산 / 직선 & 각도를 기준으로 거리 계산
+        geoPoint = userLocationUpdate.findGeoPoint("인천 연수구 아카데미로 119");
+
+        Location start = new Location("point A");
+        start.setLatitude(35.228545);
+        start.setLongitude(128.889352);
+        Location end = new Location("point B");
+        end.setLatitude(geoPoint.targetLatitude);
+        end.setLongitude(geoPoint.targetLongitude);
+
+        float distant = start.distanceTo(end);
+        Toast.makeText(mContext, "distant = " + distant, Toast.LENGTH_LONG).show();
+        */
+        transaction.replace(R.id.linear_layout, fragmentStoreHome);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+
 }
--- app/src/main/java/com/example/kioskclient/MainActivity.java
+++ app/src/main/java/com/example/kioskclient/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.kioskclient;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.skt.Tmap.TMapView;
 
 import org.json.JSONObject;
 
@@ -24,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
     private FragmentMenuCart fragmentMenuCart;
     private FragmentMenuMyPage fragmentMenuMyPage;
     private FragmentMenuOrderHistory fragmentMenuOrderHistory;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -101,7 +115,8 @@ public class MainActivity extends AppCompatActivity {
         transaction.commitAllowingStateLoss();
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-
+        TMapView tMapView = new TMapView(this);
+        tMapView.setSKTMapApiKey("5f61e3c4-b3e2-4b87-9d43-26fab3b5e095");
     }
 
     @Override
@@ -116,4 +131,100 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "현재 로그인된 사용자 : " + currentUser.getEmail(), Toast.LENGTH_SHORT).show();
         }
     }
+
+    // 앱 실행 시 gps 서비스 활성화를 위해 출력되는 dialog
+    public void showDialogForLocationServiceSetting(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("위치 서비스 활성화 필요");
+        builder.setMessage("위치 서비스가 비활성화 되어 있습니다."+ "\n" + "위치 설정을 변경할까요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent callGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+
+                //맨 하단의 onActivityResult에 결과 전송
+                startActivityForResult(callGPS, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    // 현재 location service와 gps 서비스가 실행되고 있는지 확인
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    //현재 런타임 퍼미션 확인을 위함 함수
+    public void checkRunTimePermission(){
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+            // 3.  위치 값을 가져올 수 있음
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+        }
+    }
+
+    // startActivityForResult 함수 실행 시 resultCode를 받아서 상태 확인하는 부분
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+
+                break;
+        }
+    }
+
 }
